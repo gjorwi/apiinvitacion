bec9ad03a24f1bc1197044bbd2d5aec2b4895637
jorwi garcia <jgjg.0503@gmail.com>
2026-07-24 15:05:56 -0400

    dashboard fixed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const triviaRoutes = require("./routes/trivia");
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: ["http://localhost:3000", "http://localhost:3001"] }));
 app.use(express.json({ limit: "50mb" }));
 app.use("/uploads", express.static("uploads"));
 
